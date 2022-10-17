c063567061abdd755a8d64e22a0ef1921a25b259
Jacob <procoder165@gmail.com>
2022-10-16 23:46:43 -0400

    working on making dist file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,15 +14,36 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin"
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      })
     ],
 
     module: {
       rules: [
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/rescource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            }
+          }
+        }
         
       ],
     },
